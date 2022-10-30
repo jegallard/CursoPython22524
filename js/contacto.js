73e1d1e6dff7a4fb4089c2b987e79188f6be0713
jegallard <jegallard@gmail.com>
2022-10-30 19:40:42 -0300

    diseño del sitio por Maria G y edición de pagina de contacto

---
+++ js/contacto.js
@@ -0,0 +1,23 @@
+function validateForm(){
+    return true
+    /*
+    var URL = "https://api.whatsapp.com/send?phone="
+    var telefono = "593982210224"
+    var textoNombre = "&text=Hola%20soy%20"
+    var nombre = document.getElementById("inputNombre").value
+    var textoMail = "%20mi%20email%20es%20"
+    var mail = document.getElementById("inputEmail").value
+    var textoReason = "%20y%20"
+    var reason = document.getElementById("inputReason").value
+    alert("Usted será redireccionado a Whatsapp")
+    var final = URL+telefono+textoNombre+nombre+textoMail+mail+textoReason+reason
+    alert(final)
+    //window.location.replace(final);
+    //window.location.replace("http://stackoverflow.com");
+    //window.location.href = "http://stackoverflow.com";
+    location.replace("https://www.w3schools.com")
+    location.href("https://www.w3schools.com")
+    location.assign(final)
+    //document.location.href="http://manouvellepage.com"
+    */
+}
