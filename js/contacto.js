f3a9b885e66ea24a1fbd1f755b2317efadca5444
jegallard <jegallard@gmail.com>
2022-10-31 22:02:52 -0300

    redirect whatsapp en contacto

--- js/contacto.js
+++ js/contacto.js
@@ -1,24 +1,19 @@
 function validateForm(){
-    var URL = "https://api.whatsapp.com/send?phone="
-    var telefono = "541169253443"
-    var textoNombre = "&text=Hola%20soy%20"
-    var nombre = document.getElementById("inputNombre").value
-    var textoMail = "%20mi%20email%20es%20"
-    var mail = document.getElementById("inputEmail").value
-    var textoReason = "%20y%20"
-    var reason = document.getElementById("inputReason").value
-    alert("Usted será redireccionado a Whatsapp")
-    var final = URL+telefono+textoNombre+nombre+textoMail+mail+textoReason+reason
-    alert(final)
-    //window.location.replace(final);
-    //alert("windows.location")
-    window.location.replace("http://stackoverflow.com");
-    //alert("windows.location.href")
-    window.location.href = "http://stackoverflow.com";
-    //alert("location.replace")
-    location.replace("http://stackoverflow.com")
-    //alert("location.href")
-    location.href("http://stackoverflow.com")
-    //location.assign(final)
-    //document.location.href="http://manouvellepage.com"
+    //alert("validar formulario")
+}
+
+function submitForm(){
+    var URL = "https://api.whatsapp.com/send?phone=";
+    var telefono = "541169253443";
+    var textoNombre = "&text=Hola%20soy%20";
+    var nombre = document.getElementById("inputNombre").value;
+    var textoMail = "%20mi%20email%20es%20";
+    var mail = document.getElementById("inputEmail").value;
+    var textoReason = "%20y%20";
+    var reason = document.getElementById("inputReason").value;
+    //alert("Usted será redireccionado a Whatsapp");
+    var final = URL+telefono+textoNombre+nombre+textoMail+mail+textoReason+reason;
+    //alert(final);
+    //alert("windows.location.replace");
+    window.location.replace(final);
 }
